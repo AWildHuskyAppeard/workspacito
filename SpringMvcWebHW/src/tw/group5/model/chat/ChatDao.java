46f5cccfe4d96d7757ba1d7120325b9598d62007
joeyen17 <joemail17@gmail.com>
2021-06-08 15:02:03 +0800

    Update ChatDao.java

--- SpringMvcWebHW/src/tw/group5/model/chat/ChatDao.java
+++ SpringMvcWebHW/src/tw/group5/model/chat/ChatDao.java
@@ -1,5 +1,15 @@
 package tw.group5.model.chat;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("chatDao")
+@Transactional
 public class ChatDao {
+	@Autowired
+	private SessionFactory sessionFactory;
 
 }
